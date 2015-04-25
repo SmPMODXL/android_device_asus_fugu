18b30606d0afe46e9ba04f5efd33c382ec1dbfa2
dhacker29 <dhackerdvm@gmail.com>
2015-04-25 00:09:14 -0400

    prop: Set mtp as the default usb config
    Change-Id: Id1b488a85afa3d7bdef6decaea5457a73e734792

--- device.mk
+++ device.mk
@@ -83,6 +83,9 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
 # Set the prop to enable arm native bridge
 ADDITIONAL_DEFAULT_PROPERTIES += ro.dalvik.vm.native.bridge=libhoudini.so
 
+# Set default USB interface
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp
+
 # Include AppDrawer
 PRODUCT_PACKAGES += \
     AppDrawer
